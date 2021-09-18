ece86087916147641d56c6cdac24b8633849122a
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-09-18 01:00:29 -0400

    stylings

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
   let result = 0;
-  if (fromN <= toN){
-    result = fromN + sum(fromN+1, toN);
+  if (fromN <= toN) {
+    result = fromN + sum(fromN + 1, toN);
   }
   return result;
 }
